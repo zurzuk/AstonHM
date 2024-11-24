5ff2c73895124138ed2bab528153a6a77bf979a2
Adam <ericdubaev@gmail.com>
2024-11-24 19:38:49 +0300

    Add second task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,22 @@ public class Main {
     public static void main(String[] args) {
         //Задание1
         printThreeWords();
+        //Задание2
+        checkSumSign();
 
     }
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = 256;
+        int b = 526;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
